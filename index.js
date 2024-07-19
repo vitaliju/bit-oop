7c820b26d770f6f6accc79de94c3714f8dbab46d
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-07-19 08:34:38 +0300

    dog oop

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-console.log(`..DEMO...`);
-console.log(`..DEMO...`);
-console.log(`..DEMO...`);
-console.log(`..DEMO...`);
+import { Dog } from "./js/Dog.js";
+
+const rexas = new Dog("Rexas", "black", 4);
+console.log(rexas);
+
+console.log(rexas.hi());
+console.log(rexas.think());
+console.log(rexas.addNumbers(4, 6));
+console.log(rexas.addNumbers(4, -3));
+console.log(rexas.addNumbers(4, -5));
+console.log(rexas.manyBones(15));
+
+const brisius = new Dog("Brisius", "white", 6);
+console.log(brisius);
+
+console.log(brisius);
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.lostLeg());
+console.log(brisius.birthday());
+console.log(brisius.birthday());
+console.log(brisius.birthday());
---
+++ js/Dog.js
@@ -0,0 +1,41 @@
+export class Dog {
+  constructor(name, color, amzius) {
+    this.vardas = name;
+    this.furcolor = color;
+    this.age = amzius;
+    this.legsCount = 4;
+    this.hasTail = true;
+  }
+
+  birthday() {
+    this.age++;
+    return `Suns ${this.age} gimtadienis! 🥳`;
+  }
+
+  lostLeg() {
+    if (this.legsCount === 0) {
+      return "Suo jau turi 0 (nuli) koju... daugiau nera ko prarasti";
+    }
+    this.legsCount--;
+    return `Suo prarado koja...😭`;
+  }
+
+  hi() {
+    return "Suo sako: au au!!";
+  }
+
+  think() {
+    return "Suo galvoja: 🍖🍖🍖";
+  }
+
+  addNumbers(a, b) {
+    if (a + b < 0) {
+      return "Suo yra liudnas... negali suskaiciuoti negatyvaus kaulu skaiciaus";
+    }
+    return `Suo suskaiciavo: ${a} + ${b} = ${"🍖".repeat(a + b)}`;
+  }
+
+  manyBones(count) {
+    return `Many bones: ${"🍖".repeat(count)}`;
+  }
+}
