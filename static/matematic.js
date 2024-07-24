cf83b71a4238b9b54bad2e26b57678db992bf342
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-07-24 12:37:47 +0300

    Validation file

---
+++ static/matematic.js
@@ -0,0 +1,14 @@
+export const matematic = {
+    add: (a, b) => a + b,
+    diff: (a, b) => a - b,
+    multi: (a, b) => a * b,
+    div: (a, b) => a / b,
+    mod: (a, b) => a % b,
+};
+
+
+console.log(matematic.add(5, 6));
+console.log(matematic.diff(5, 6));
+console.log(matematic.multi(5, 6));
+console.log(matematic.div(5, 6));
+console.log(matematic.mod(5, 6));
